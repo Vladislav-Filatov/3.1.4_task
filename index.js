38013e515f3851bbf374ab3421634eff7f3b1892
vlad_filatov <vf140704@gmail.com>
2025-12-25 18:21:56 +0300

    блок 1.5

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 let swiper = null;
+const repairBrandsButton = document.querySelector('.repair-brands__read-more-button');
+const repairBrandsButtonText = document.querySelector('.read-more-button__text');
+const repairBrandsButtonImage = document.querySelector('.read-more-button__image')
+const swiperWrapper = document.querySelector('.swiper-wrapper');
 
 function initSwiper() {
     swiper = new Swiper('.swiper', {
@@ -16,18 +20,31 @@ function initSwiper() {
 function destroySwiper() {
     if (swiper) {
         swiper.destroy();
-        swiper=null;
+        swiper = null;
     }
 }
 
 function checkSize() {
-    if (window.innerWidth<768) {
+    if (window.innerWidth<768 && !swiper) {
         initSwiper();
-    }
-    else if (swiper) {
+    } else if (window.innerWidth>=768 && swiper) {
         destroySwiper();
     }
 }
+
+const brandsVisibility = () => {
+    if (swiperWrapper.classList.contains('expanded')) {
+        swiperWrapper.classList.remove('expanded');
+        repairBrandsButtonImage.classList.remove('expanded');
+        repairBrandsButtonText.textContent = 'Показать все';
+    } else {
+        swiperWrapper.classList.add('expanded');
+        repairBrandsButtonImage.classList.add('expanded');
+        repairBrandsButtonText.textContent = 'Скрыть';
+    }
+};
+
+repairBrandsButton.addEventListener('click',  brandsVisibility);
 
 window.addEventListener('load', checkSize);
 window.addEventListener('resize', checkSize);
